cfe7d083faa4c6b08b4399620259e1c92641f5b7
MrBalintore <davidjohnjohnston@gmail.com>
2026-01-24 20:42:55 +0000

    synchronise 24th Jan

---
+++ Basement Coving/calculate_coving.py
@@ -0,0 +1,20 @@
+
+def calculate_bedroom():
+    all = 0
+    return all
+
+def calculate_living_room():
+    near_wall = (265 + 318)
+    far_wall = 350 + 190
+    left_wall = 434
+    right_wall = 250 + 69 + 14 + 116 + 173
+    straight_right_wall = 250 + 14 + 173
+    all = near_wall + far_wall + left_wall + right_wall
+    return all
+
+def main():
+    living = calculate_living_room()
+
+if __name__ == "__main__":
+    main()
+
